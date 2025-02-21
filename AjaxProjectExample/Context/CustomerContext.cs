a7d2862847f0d68094966873dbbe49f728a87eef
can gündoğdu <gundogdu.can@hotmail.com>
2025-02-21 22:00:37 +0300

    Add project files.

---
+++ AjaxProjectExample/Context/CustomerContext.cs
@@ -0,0 +1,15 @@
+﻿using AjaxProjectExample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProjectExample.Context
+{
+    public class CustomerContext:DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=CAN\\SQLEXPRESS;initial Catalog=CustomerAjaxDb;integrated security=true; trustServerCertificate=true");
+        }
+        public DbSet<Customer> Customers { get; set; }
+        
+    }
+}
---
+++ AjaxProjectExample/Controllers/CustomerController.cs
@@ -0,0 +1,60 @@
+﻿using AjaxProjectExample.Context;
+using AjaxProjectExample.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Storage.Json;
+using Newtonsoft.Json;
+
+namespace AjaxProjectExample.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private readonly CustomerContext _context;
+
+        public CustomerController(CustomerContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CustomerList()
+        {
+            var values = _context.Customers.ToList();
+            var jsonValues= JsonConvert.SerializeObject(values);
+            return Json(jsonValues);
+        }
+
+        public IActionResult CreateCustomer(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+            var jsonValue= JsonConvert.SerializeObject(customer);
+            return Json(jsonValue);
+        }
+
+        public IActionResult DeleteCustomer(int id)
+        {
+            var value= _context.Customers.Find(id);
+            _context.Customers.Remove(value);
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        public IActionResult GetCustomer(int id)
+        {
+            var value = _context.Customers.Find(id);
+            var jsonValue = JsonConvert.SerializeObject(value);
+            return Json(jsonValue);
+        }
+
+        public IActionResult UpdateCustomer(Customer customer)
+        {
+            _context.Customers.Update(customer);
+            _context.SaveChanges();   
+            return NoContent();
+        }
+    }
+}
